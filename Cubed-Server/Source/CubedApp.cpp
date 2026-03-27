6986bb5aa83a31b8c7b665314855d1a0f39ab2c0
developerjarvis <abhinavsharma27852862@gmail.com>
2026-03-27 15:50:01 +0530

    Cubed server ready

--- Cubed-Server/Source/CubedApp.cpp
+++ Cubed-Server/Source/CubedApp.cpp
@@ -1,56 +1,23 @@
 #include "Walnut/Application.h"
 #include "Walnut/EntryPoint.h"
 
-#include "Walnut/Image.h"
-#include "Walnut/UI/UI.h"
+#include "Walnut/Core/Log.h"
+#include <chrono>
 
 class ExampleLayer : public Walnut::Layer
 {
 public:
-	virtual void OnUIRender() override
+	virtual void OnAttach() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
-		ImGui::End();
 
-		ImGui::ShowDemoWindow();
-
-		UI_DrawAboutModal();
 	}
-
-	void UI_DrawAboutModal()
+	
+	virtual void OnUpdate(float ts) override
 	{
-		if (!m_AboutModalOpen)
-			return;
+		WL_INFO("Server Timestep: {}", ts);
 
-		ImGui::OpenPopup("About");
-		m_AboutModalOpen = ImGui::BeginPopupModal("About", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
-		if (m_AboutModalOpen)
-		{
-			auto image = Walnut::Application::Get().GetApplicationIcon();
-			ImGui::Image(image->GetDescriptorSet(), { 48, 48 });
-
-			ImGui::SameLine();
-			Walnut::UI::ShiftCursorX(20.0f);
-
-			ImGui::BeginGroup();
-			ImGui::Text("Walnut application framework");
-			ImGui::Text("by Studio Cherno.");
-			ImGui::EndGroup();
-
-			if (Walnut::UI::ButtonCentered("Close"))
-			{
-				m_AboutModalOpen = false;
-				ImGui::CloseCurrentPopup();
-			}
-
-			ImGui::EndPopup();
-		}
-	}
-
-	void ShowAboutModal()
-	{
-		m_AboutModalOpen = true;
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(5ms);
 	}
 private:
 	bool m_AboutModalOpen = false;
@@ -59,31 +26,10 @@ private:
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
 	Walnut::ApplicationSpecification spec;
-	spec.Name = "Walnut Example";
-	spec.CustomTitlebar = true;
+	spec.Name = "Cubed Server";
 
 	Walnut::Application* app = new Walnut::Application(spec);
 	std::shared_ptr<ExampleLayer> exampleLayer = std::make_shared<ExampleLayer>();
 	app->PushLayer(exampleLayer);
-	app->SetMenubarCallback([app, exampleLayer]()
-	{
-		if (ImGui::BeginMenu("File"))
-		{
-			if (ImGui::MenuItem("Exit"))
-			{
-				app->Close();
-			}
-			ImGui::EndMenu();
-		}
-
-		if (ImGui::BeginMenu("Help"))
-		{
-			if (ImGui::MenuItem("About"))
-			{
-				exampleLayer->ShowAboutModal();
-			}
-			ImGui::EndMenu();
-		}
-	});
 	return app;
 }
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Cubed
+
+### Build in Visual Studio
+
+```bash
+Ctrl + Shift + B
+```
+
+### Run in Visual Studio
+
+```bash
+F5
+```
+
+## Errors
+
+1. MVSC Error
+   1. Visual Studio Installer -> select Desktop development with C++
+   2. make sure these are checked
+      - MVSC vXXX build tools
+      - Windows 10/ 11 SDK
+   3. Retarget project
+      - Right click solution -> Retarget solution
+      - Choose available toolset
+
+2. Vulkan SDK error
+   1. Make sure Vulkan SDK is installed on device
+   2. Add to System Variables: VULKAN_SDK
+   3. Rerun Setup.bat > Reload All
